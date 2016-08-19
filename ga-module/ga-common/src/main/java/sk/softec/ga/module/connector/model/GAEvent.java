6e8b80b4d98a9e74935e8fe6120e2501f526e801
jankovj <jaroslav.jankovic@softec.sk>
2016-08-19 13:00:15 +0200

    Another commit

---
+++ ga-module/ga-common/src/main/java/sk/softec/ga/module/connector/model/GAEvent.java
@@ -0,0 +1,7 @@
+package sk.softec.ga.module.connector.model;
+
+/**
+ * Created by jankovj on 19. 8. 2016.
+ */
+public class GAEvent {
+}
--- ga-module/ga-common/src/main/java/sk/softec/ga/module/connector/model/GaEvent.java
+++
@@ -1,7 +0,0 @@
-package sk.softec.ga.module.connector.model;
-
-/**
- * Created by jankovj on 17. 8. 2016.
- */
-public class GaEvent {
-}
